4e2efd0dbe08e4ea4f70256ab54283215218ae24
kangdain <juhyueon3@naver.com>
2021-01-26 01:39:22 +0900

    add social login

--- src/fbase.js
+++ src/fbase.js
@@ -13,5 +13,7 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+export const firebaseInstance = firebase;
+
 export const authService = firebase.auth();
 
